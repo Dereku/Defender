0b35ace7ecd101a0ba6a20d471f472615ce6bc42
Dereku <dereku@default.im>
2016-04-10 17:04:58 +0700

    Messages and StatsCommand changes.

--- ru/Sonicxd2/PubDefender/Messages.java
+++ ru/Sonicxd2/PubDefender/Messages.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Заголовок лицензии. Удали его, если ты не выбрал лицензию.
  */
 package ru.Sonicxd2.PubDefender;
 
@@ -12,12 +10,17 @@ import org.bukkit.ChatColor;
  * @author Yaroslav
  */
 public class Messages {
-    public static String prefix=ChatColor.GREEN+"[Defender]";
-    public static String messageSuccess=ChatColor.GREEN+"Вы прошли проверку!";
-    public static String messageError=ChatColor.RED+"У вас осталось %number% попыток";
-    public static String kickMessage=ChatColor.RED+"Вы не прошли проверку!";
-    public static String sum=ChatColor.BOLD+"плюс ";
-    public static String min=ChatColor.BOLD+"минус ";
-    public static String umn=ChatColor.BOLD+"умножить на ";
-    public static String primer=prefix+" %num1% %znak% "+ChatColor.GREEN+" %num2%";
+
+    /* 
+        Нет, так не годится. Используй .properties или же .yml 
+        (в крайнем случае) вместо этого. 
+    */
+    public static String prefix = ChatColor.GREEN + "[Defender]";
+    public static String messageSuccess = ChatColor.GREEN + "Вы прошли проверку!";
+    public static String messageError = ChatColor.RED + "У вас осталось %number% попыток";
+    public static String kickMessage = ChatColor.RED + "Вы не прошли проверку!";
+    public static String sum = ChatColor.BOLD + "плюс ";
+    public static String min = ChatColor.BOLD + "минус ";
+    public static String umn = ChatColor.BOLD + "умножить на ";
+    public static String primer = prefix + " %num1% %znak% " + ChatColor.GREEN + " %num2%";
 }
--- ru/Sonicxd2/PubDefender/StatsCommand.java
+++ ru/Sonicxd2/PubDefender/StatsCommand.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Заголовок лицензии. Удали его, если ты не выбрал лицензию.
  */
 package ru.Sonicxd2.PubDefender;
 
@@ -15,20 +13,20 @@ import org.bukkit.entity.Player;
  *
  * @author Yaroslav
  */
-public class StatsCommand implements CommandExecutor{
+public class StatsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
-        if(cs instanceof Player){
-            cs.sendMessage(ChatColor.GREEN+"Статистика для консоли.");
+        if (cs instanceof Player) {
+            cs.sendMessage(ChatColor.GREEN + "Статистика для консоли.");
             return true;
         }
-        cs.sendMessage(Messages.prefix+" Defender версия - "+Main.getInstance().version);
-        cs.sendMessage(Messages.prefix+" Статистика:");
-        cs.sendMessage(Messages.prefix+" Всего игроков зашло: "+Main.getInstance().allPlayers.toString());
-        cs.sendMessage(Messages.prefix+" Игроков не прошедших проверку: "+(Main.getInstance().allPlayers-Main.getInstance().playersInDef));
-        cs.sendMessage(Messages.prefix+" Игроков прошедших проверку:"+Main.getInstance().playersInDef);
+        cs.sendMessage(Messages.prefix + " Defender версия - " + Main.getInstance().version);
+        cs.sendMessage(Messages.prefix + " Статистика:");
+        cs.sendMessage(Messages.prefix + " Всего игроков зашло: " + Main.getInstance().allPlayers.toString());
+        cs.sendMessage(Messages.prefix + " Игроков не прошедших проверку: " + (Main.getInstance().allPlayers - Main.getInstance().playersInDef));
+        cs.sendMessage(Messages.prefix + " Игроков прошедших проверку:" + Main.getInstance().playersInDef);
         return true;
     }
-    
+
 }
